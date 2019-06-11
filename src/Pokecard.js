580fc7c1e48a236f3ccf73972741de60a9ee4063
bsummers <bryan.summers@insight.com>
2019-06-11 10:04:20 +0800

    created pokegame component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './Pokecard.scss';
 
-const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
 
 class Pokecard extends Component {
     static defaultProps = {
@@ -10,11 +10,24 @@ class Pokecard extends Component {
         type: 'Omega Level Son',
         base_experience: 999
     }
+
+    padWithZeroes = (num) => {
+        let id = num.toString();
+        
+        if (id.length === 2) {
+            return `0${id}`; 
+        } else if (id.length === 1) {
+            return `00${id}`;
+        } else {
+            return id;
+        }
+    }
+
     render() {
         return (
             <div className="Pokecard">
                 <h2>{this.props.name}</h2>
-                <img src={`${POKE_API}${this.props.id}.png`}/>
+                <img src={`${POKE_API}${this.padWithZeroes(this.props.id)}.png`} alt={`${this.props.name}`}/>
                 <p>{this.props.type}</p>
                 <p>{this.props.base_experience}</p>
             </div>
---
+++ src/Pokegame.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Pokedex from './Pokedex'
+
+
+class Pokegame extends Component {
+    render() {
+        return (
+            
+        );
+    }
+  
+}
+
+export default Pokegame;
